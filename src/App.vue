d9717585246db786ef258515f2ab20f7663b3d2e
Abdul Tanwar <abdul.tanwar@sportzinteractive.com>
2024-09-11 12:43:42 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/PlayerList.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style></style>
